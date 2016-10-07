3f08d302a8c407859cfba5c7fc7f9dbd5d97d2e7
qyb225 <qyb225@gmail.com>
2016-10-07 16:24:17 +0800

    Fixed some bugs

--- Homework4/calculate.js
+++ Homework4/calculate.js
@@ -1,128 +1,173 @@
 express = "";
 solution = 0;
 finish = 0;
+
 function click(i) {
     var show_ex = document.getElementById("screen_u");
-    var show_sl = document.getElementById("screen_d");
-    function express_hidden(str) {
-        if (str.length > 25)
-            return str.substring(str.length - 25);
-        else
-            return str;
-    }
+    var show_sl = document.getElementById("screen_d");    
     if (i == "<") {
-        if (finish == 1) {
-            finish = 0;
-        }
-        if (0 < express.length) {
-            express = express.substring(0, express.length - 1);
-        }
+        __back__();
         show_ex.innerHTML = express_hidden(express);
     }
     else if (i == "ce") {
-        express = "";
+        __init__();
         show_ex.innerHTML = express;
-        solution = 0;
         show_sl.innerHTML = solution;
-        finish = 0;
     }
     else if (i == "=") {
-        function num_tackle(n) {
-            var num = n + "";
-            var sit = num.indexOf(".");
-            var del = 0;
-            var output = "";
-            if (sit != -1) {
-                for (var j = num.length - 1; j > sit; j--) {
-                    if (num[j] == "0") {
-                        del += 1;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                num = num.substring(0, num.length - del);
-                return parseFloat(num);
-            }
-            else {
-                return n;
-            }
-        }
-        var magnitude = 1;
-        var count = 0;
-        try {
-            solution = eval(express);
-        }
-        catch(err) {
-            if(err.name == "SyntaxError") {
-                solution = NaN;
-            }
-        }
-     
-        //solution = eval(express);
-        if (Math.abs(solution) > 1) {
-            while (Math.abs(solution) / magnitude >= 10) {
-                count++;
-                magnitude *= 10;
-            }
-        }
-        else if (solution == 0) {
-            count = 0;
-            magnitude = 1;
-        }
-        else {
-            while (Math.abs(solution) * magnitude < 1) {
-                count--;
-                magnitude *= 10;
-            }
-        }
-        if (count <= 9 && count >= -5) {
-            solution = solution.toFixed(11 - Math.abs(count));
-            solution = num_tackle(solution);
-            output = solution + "";
-        }
-        else if (count < 0) {
-            var bottom_num = solution * magnitude;
-            var level = count;
-            bottom_num = bottom_num.toFixed(7);
-            bottom_num = num_tackle(bottom_num);
-            solution = num_tackle(solution);
-            output = bottom_num + "e" + level;
-        }
-        else {
-            var bottom_num = solution / magnitude;
-            var level = count;
-            bottom_num = bottom_num.toFixed(8);
-            bottom_num = num_tackle(bottom_num);
-            solution = num_tackle(solution);
-            output = bottom_num + "e" + level;
-        }
-
-        if (Math.abs(count) >= 50) {
-            output = "Math Error!";
-            solution = 0;
-            finish = 0;
-        }
-        if (solution == "NaN")
-            output = "Input Error!";
-        show_sl.innerHTML = output;     
-        finish = 1;
+        var output = __calculate__();
+        show_sl.innerHTML = output;
     }
     else {
-        if (1 == finish) {
-            if (i == "+" || i == "-" || i == "*" || i == "/") {
-                express =  "" + solution;
-            }
-            else {
-                express = "";
-            }
-            solution = 0;
-            finish = 0;
-        }
-        express += i;
+        __addstr__(i);
         show_ex.innerHTML = express_hidden(express);
     }    
 }
+
+function __back__() {
+    finish = 0;
+    if (0 < express.length)
+        express = express.substring(0, express.length - 1);
+    return;
+}
+
+function express_hidden(str) {
+    if (str.length > 25)
+        return str.substring(str.length - 25);
+    else
+        return str;
+}
+
+
+function __init__() {
+    express = "";
+    solution = 0;
+    finish = 0;
+}
+
+function num_tackle(n) {
+    var num = n + "";
+    var sit = num.indexOf(".");
+    var sit0 = num.indexOf("e");
+    var del = 0;
+    var output = "";
+    if (sit0 != -1)
+        return n;
+    else if (sit != -1) {
+        for (var j = num.length - 1; j > sit; j--) {
+            if (num[j] == "0") {
+                del += 1;
+            }
+            else {
+                break;
+            }
+        }
+        num = num.substring(0, num.length - del);
+        return parseFloat(num);
+    }
+    else
+        return n;
+}
+
+function __addstr__(i) {
+    if (1 == finish) {
+        if (i == "+" || i == "-" || i == "*" || i == "/") {
+            express =  "" + solution;
+        }
+        else {
+            express = "";
+        }
+        solution = 0;
+        finish = 0;
+    }
+    express += i;
+}
+
+function __calculate__() {
+    var magnitude = 1;
+    var count = 0;
+    try {
+        solution = eval(express);
+
+        for(var k = 0; k < express.length - 1; ++k) {
+            if (express[k] == express[k + 1] && express[k] == "/") {
+                throw SyntaxError;
+                break;
+            }
+        }
+    }
+    catch(err) {
+        if(err.name == "SyntaxError") {
+            solution = NaN;
+        }
+        else if (err.name == "EvalError") {
+            solution = NaN;
+        }
+        else if (err.name == "ReferenceError") {
+            solution = NaN;
+        }
+    }
+     
+    if (Math.abs(solution) > 1) {
+        while (Math.abs(solution) / magnitude > 10) {
+            count++;
+            magnitude *= 10;
+        }
+    }
+    else if (solution == 0) {
+        count = 0;
+        magnitude = 1;
+    }
+    else {
+        while (Math.abs(solution) * magnitude < 1) {
+            count--;
+            magnitude *= 10;
+        }
+    }
+    //begin
+    if (count <= 9 && count >= -5) {
+        solution = solution.toFixed(11 - Math.abs(count));
+        solution = num_tackle(solution);
+        output = solution + "";
+    }
+    else if (count < 0) {
+        var bottom_num = solution * magnitude;
+        var level = count;
+        if (count <= -10) {
+            var solution_bottom = bottom_num.toFixed(15);
+            solution_bottom = num_tackle(solution_bottom);
+            solution = "" + solution_bottom + "e" + level;
+        }
+        bottom_num = bottom_num.toFixed(7);
+        bottom_num = num_tackle(bottom_num);
+        solution = num_tackle(solution);
+        output = bottom_num + "e" + level;
+    }
+    else {
+        var bottom_num = solution / magnitude;
+        var level = count;
+        if (count >= 10) {
+            var solution_bottom = bottom_num.toFixed(15);
+            solution_bottom = num_tackle(solution_bottom);
+            solution = "" + solution_bottom + "e" + level;
+        }
+        bottom_num = bottom_num.toFixed(8);
+        bottom_num = num_tackle(bottom_num);
+        solution = num_tackle(solution);
+        output = bottom_num + "e" + level;
+    }
+    //end
+    if (Math.abs(count) >= 50) {
+        output = "Math Error!";
+        solution = 0;
+        finish = 0;
+    }
+    if (solution == "NaN")
+        output = "Input Error!";    
+    finish = 1;
+    return output;
+}
+
 
 var press0 = document.getElementById("_0");
 var press1 = document.getElementById("_1");
