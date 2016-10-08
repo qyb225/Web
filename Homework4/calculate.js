ae77f3cb2b7c8e4788dc1bf7ca289a2fd0d9ec7a
qyb225 <qyb225@gmail.com>
2016-10-08 21:43:28 +0800

    Fixed some bugs

--- Homework4/calculate.js
+++ Homework4/calculate.js
@@ -1,29 +1,41 @@
-express = "";
-solution = 0;
-finish = 0;
+express = ""; //Save expression
+solution = 0; //Save solution
+finish = 0;   //To judge if calculation is completed.
 
 function click(i) {
     var show_ex = document.getElementById("screen_u");
     var show_sl = document.getElementById("screen_d");    
+    //Click "Back" Button
     if (i == "<") {
-        __back__();
+        __back__(); //Clear one letter or symbol.
         show_ex.innerHTML = express_hidden(express);
     }
+    //Click "CE" Button
     else if (i == "ce") {
-        __init__();
+        __init__(); //Initialize expression and solution.
         show_ex.innerHTML = express;
         show_sl.innerHTML = solution;
     }
+    //Click "Calculate" Button
     else if (i == "=") {
-        var output = __calculate__();
+        var output = __calculate__(); //Calculate expression, analysing solution, exception handling.
         show_sl.innerHTML = output;
     }
+    //Click other Button.
     else {
         __addstr__(i);
         show_ex.innerHTML = express_hidden(express);
     }    
 }
 
+//Initialize the expression and solution.
+function __init__() {
+    express = "";
+    solution = 0;
+    finish = 0;
+}
+
+//Clear one letter or symbol.
 function __back__() {
     finish = 0;
     if (0 < express.length)
@@ -31,48 +43,11 @@ function __back__() {
     return;
 }
 
-function express_hidden(str) {
-    if (str.length > 25)
-        return str.substring(str.length - 25);
-    else
-        return str;
-}
-
-
-function __init__() {
-    express = "";
-    solution = 0;
-    finish = 0;
-}
-
-function num_tackle(n) {
-    var num = n + "";
-    var sit = num.indexOf(".");
-    var sit0 = num.indexOf("e");
-    var del = 0;
-    var output = "";
-    if (sit0 != -1)
-        return n;
-    else if (sit != -1) {
-        for (var j = num.length - 1; j > sit; j--) {
-            if (num[j] == "0") {
-                del += 1;
-            }
-            else {
-                break;
-            }
-        }
-        num = num.substring(0, num.length - del);
-        return parseFloat(num);
-    }
-    else
-        return n;
-}
-
+//If a peroid of calculation is over, judge if we want to use the solution.
 function __addstr__(i) {
     if (1 == finish) {
-        if (i == "+" || i == "-" || i == "*" || i == "/") {
-            express =  "" + solution;
+        if (i == "+" || i == "-" || i == "*" || i == "/") { //When next symbol is "+-/*" of "Back"
+            express =  "" + solution;                       //We need to reuse the solution.
         }
         else {
             express = "";
@@ -83,10 +58,11 @@ function __addstr__(i) {
     express += i;
 }
 
+//Calculating function.
 function __calculate__() {
-    var magnitude = 1;
-    var count = 0;
-    try {
+    var magnitude = 1; //The magnitude of the number, E.g. 3214, its magnitude is 1000;
+    var count = 0; //The level of a number.
+    try { //Judge if the expression is legal.
         solution = eval(express);
 
         for(var k = 0; k < express.length - 1; ++k) {
@@ -107,9 +83,10 @@ function __calculate__() {
             solution = NaN;
         }
     }
-     
-    if (Math.abs(solution) > 1) {
-        while (Math.abs(solution) / magnitude > 10) {
+
+    //Calculate the level and magnitude.Then judge if we need "Scientific Notation"
+    if (Math.abs(solution) > 1) { 
+        while (Math.abs(solution) / magnitude >= 10) {
             count++;
             magnitude *= 10;
         }
@@ -124,12 +101,18 @@ function __calculate__() {
             magnitude *= 10;
         }
     }
-    //begin
+
+    //1. Don't need to transform to "Scientific Notation"
     if (count <= 9 && count >= -5) {
-        solution = solution.toFixed(11 - Math.abs(count));
-        solution = num_tackle(solution);
-        output = solution + "";
+        output = solution;
+        if (Math.abs(output) < 1)
+            output = output.toFixed(11);
+        else
+            output = output.toFixed(11 - Math.abs(count));
+        output = num_tackle(output);
+        output = output + "";
     }
+    //2. Too small that we need to transform to "Scientific Notation"
     else if (count < 0) {
         var bottom_num = solution * magnitude;
         var level = count;
@@ -143,6 +126,7 @@ function __calculate__() {
         solution = num_tackle(solution);
         output = bottom_num + "e" + level;
     }
+    //3. Too large that we need to transform to "Scientific Notation"
     else {
         var bottom_num = solution / magnitude;
         var level = count;
@@ -156,19 +140,52 @@ function __calculate__() {
         solution = num_tackle(solution);
         output = bottom_num + "e" + level;
     }
-    //end
+    //If the level is lager than 50 or less than -50, We don't need to calculate.
     if (Math.abs(count) >= 50) {
         output = "Math Error!";
         solution = 0;
         finish = 0;
     }
-    if (solution == "NaN")
+    if (solution + "" == "NaN" || output + "" == "NaN") {
         output = "Input Error!";    
+    }
     finish = 1;
     return output;
 }
 
+//To let screen not overflow and move to the left.
+function express_hidden(str) {
+    if (str.length > 25)
+        return str.substring(str.length - 25);
+    else
+        return str;
+}
 
+//Tackle the accuracy of the number. Turn 0.3000000000001 to 0.3 etc.
+function num_tackle(n) {
+    var num = n + "";
+    var sit = num.indexOf(".");
+    var sit0 = num.indexOf("e");
+    var del = 0;
+    var output = "";
+    if (sit0 != -1)
+        return n;
+    else if (sit != -1) {
+        for (var j = num.length - 1; j > sit; j--) {
+            if (num[j] == "0")
+                del += 1;
+            else
+                break;
+        }
+        num = num.substring(0, num.length - del);
+        return parseFloat(num);
+    }
+    else
+        return n;
+}
+
+
+//get all elements.
 var press0 = document.getElementById("_0");
 var press1 = document.getElementById("_1");
 var press2 = document.getElementById("_2");
@@ -190,7 +207,7 @@ var bracket_r = document.getElementById("bracket_r")
 var ce = document.getElementById("ce");
 var equal = document.getElementById("equal");
 
-
+//When press a button...
 press0.onclick = function() {click("0");}
 press1.onclick = function() {click("1");}
 press2.onclick = function() {click("2");}
