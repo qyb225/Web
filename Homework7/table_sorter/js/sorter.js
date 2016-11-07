c55cb3032b77f4fc383b8e7a755e1436631e7934
Qubic <qyb225@gmail.com>
2016-11-07 21:26:51 +0800

    Update sorter.js

--- Homework7/table_sorter/js/sorter.js
+++ Homework7/table_sorter/js/sorter.js
@@ -1,1 +1,21 @@
-var what_to_sort = $("th");
+var what_to_sort;
+var need_to_sort_1;
+var need_to_sort_2;
+
+window.onload = function() {
+	what_to_sort = $("th");
+	need_to_sort_1 = $("#todo td");
+	need_to_sort_2 = $("#staff td");
+	for (var i = 0; i < 6; ++i) {
+		what_to_sort.eq(i).attr("class", i + "");
+		what_to_sort.eq(i).click(function() {judge(i)});
+	}
+	for (var i = 0; i < 9; ++i) {
+		need_to_sort_1.eq(i).attr("class", (i % 3) + "");
+		need_to_sort_2.eq(i).attr("class", (i % 3 + 3) + "");
+	}
+}
+
+function judge(i) {
+	var s = $("." + i + " td");
+}
